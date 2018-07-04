47f0daa86d63fa6ddab1dab8e6f1da6763ad15d0
arashrestha <aarash428@gmail.com>
2018-07-04 18:34:31 +0545

    heroku push

--- app/assets/javascripts/reg.js
+++ app/assets/javascripts/reg.js
@@ -1,34 +1,31 @@
-alert('hi');
 
-$(document).ready(function(){
-    var name;
-    var email;
-    var regno;
-    var id;
-    $('#username').change(function(){
-        $('#email').change(function(){
-            
-            name =$('#username').val();
-            email=$('#email').val();
-            num='1234567890';
-            var ID_LENGTH = 3;
-            
-            var generate = function() {
-                var rtn = '';
-                for (var i = 0; i < ID_LENGTH; i++) {
-                    rtn += num.charAt(Math.floor(Math.random() * num.length));
+$( document ).on('turbolinks:load', function() {
+    alert('hi');
+        var name;
+        var email;
+        var regno;
+        var id;
+        $('#username').change(function(){
+            $('#email').change(function(){
+                
+                name =$('#username').val();
+                email=$('#email').val();
+                num='1234567890';
+                var ID_LENGTH = 3;
+                
+                var generate = function() {
+                    var rtn = '';
+                    for (var i = 0; i < ID_LENGTH; i++) {
+                        rtn += num.charAt(Math.floor(Math.random() * num.length));
+                    }
+                    return rtn;
                 }
-                return rtn;
-            }
+                
+                regno=name+'/'+email+'/'+generate();
+                $('#regno').val(regno);
+                
+                
+            })
             
-            regno=name+'/'+email+'/'+generate();
-            $('#regno').val(regno);
-            
-            
-        })
-        
-    })
-    
-});
-
-
+        })  
+})
