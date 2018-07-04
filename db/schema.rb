c1f562b7db600116df7eae106ca76fcc95f075a3
arashrestha <aarash428@gmail.com>
2018-07-04 20:22:44 +0545

    Update gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2018_07_04_060551) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "uniqueids", force: :cascade do |t|
